893341c0877bd5f7e7654df3a478b07c13658cb0
Santosh <santoshn91@gmail.com>
2025-03-03 16:03:59 +0530

    updated

--- src/pages/ReportManagement/InventoryManagement/StockExpiryReport.jsx
+++ src/pages/ReportManagement/InventoryManagement/StockExpiryReport.jsx
@@ -89,7 +89,7 @@ const StockExpiryReport = () => {
 
     async function fetchReport(request) {
         const response = await fetch(
-            "http://localhost:43705/api/ReportsApi/GetStockExpiryRpt",
+            "https://192.168.29.254:808/api/ReportsApi/GetStockExpiryRpt",
             {
                 method: "POST",
                 headers: {
