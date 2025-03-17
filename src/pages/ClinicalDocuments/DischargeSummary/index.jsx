d14d4dcc86a72cc47103da06daef361e7c58cd19
Santosh <santoshn91@gmail.com>
2025-03-17 16:42:59 +0530

    updated

--- src/pages/ClinicalDocuments/DischargeSummary/index.jsx
+++ src/pages/ClinicalDocuments/DischargeSummary/index.jsx
@@ -426,7 +426,7 @@ const DischargeSummary = (details) => {
 
   async function fetchReport(request) {
     const response = await fetch(
-      "http://localhost:43705/api/ReportsApi/GetDischargeSummaryRpt",
+      "https://192.168.29.254:808/api/ReportsApi/GetDischargeSummaryRpt",
       {
         method: "POST",
         headers: {
